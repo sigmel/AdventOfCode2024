9641b3aba10e5fc028b77cce09f141ab64ead6cc
sigmel <sigmel@gmail.com>
2024-12-21 13:25:50 -0600

    Day 17 - Part 2

--- Project17/Main.c
+++ Project17/Main.c
@@ -102,7 +102,7 @@ void clearVector(struct Vector* vector)
 #define OPCODE_BDV 6
 #define OPCODE_CDV 7
 
-int comboOperand(int operand, int registerA, int registerB, int registerC)
+long long comboOperand(int operand, long long registerA, long long registerB, long long registerC)
 {
 	if (operand >= 0 && operand <= 3) { return operand; }
 	if (operand == 4) { return registerA; }
@@ -126,19 +126,19 @@ void main()
 	// register A
 	fgets(input, INPUT_SIZE, filePointer);
 	while (!isdigit(input[advIndex])) { advIndex++; }
-	int registerA = atoi(&input[advIndex]);
+	long long registerA = atoi(&input[advIndex]);
 
 	// register B
 	advIndex = 0;
 	fgets(input, INPUT_SIZE, filePointer);
 	while (!isdigit(input[advIndex])) { advIndex++; }
-	int registerB = atoi(&input[advIndex]);
+	long long registerB = atoi(&input[advIndex]);
 
 	// register C
 	advIndex = 0;
 	fgets(input, INPUT_SIZE, filePointer);
 	while (!isdigit(input[advIndex])) { advIndex++; }
-	int registerC = atoi(&input[advIndex]);
+	long long registerC = atoi(&input[advIndex]);
 
 	// blank line
 	fgets(input, INPUT_SIZE, filePointer);
@@ -158,122 +158,155 @@ void main()
 	struct Vector outVector;
 	initVector(&outVector);
 
+	// Every time we loop on this, we divide A by 8, so we can sort of do that in reverse, we we find the digit we want
+	// (starting from the back), then multiply by 8, proceed to find the next digit, and so on until we have all of them
 	int instructionPointer = 0;
-	while (instructionPointer < programVector.count)
+	int numCompareDigits = 1;
+	long long initialA = 0;
+	int done = 0;
+	while (!done)
 	{
-		int opcode = programVector.data[instructionPointer];
-		switch (opcode)
+		registerA = initialA;
+		registerB = 0;
+		registerC = 0;
+		while (instructionPointer < programVector.count)
 		{
-			case OPCODE_ADV:
+			int opcode = programVector.data[instructionPointer];
+			switch (opcode)
 			{
-				// The adv instruction(opcode 0) performs division. The numerator is the value in the A register.
-				// The denominator is found by raising 2 to the power of the instruction's combo operand.
-				// (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.)
-				// The result of the division operation is truncated to an integer and then written to the A register.
-				if (instructionPointer + 1 < programVector.count)
+				case OPCODE_ADV:
 				{
-					int denominator = 1 << comboOperand(programVector.data[instructionPointer + 1], registerA, registerB, registerC);
-					registerA /= denominator;
-				}
+					// The adv instruction(opcode 0) performs division. The numerator is the value in the A register.
+					// The denominator is found by raising 2 to the power of the instruction's combo operand.
+					// (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.)
+					// The result of the division operation is truncated to an integer and then written to the A register.
+					if (instructionPointer + 1 < programVector.count)
+					{
+						long long denominator = 1LL << comboOperand(programVector.data[instructionPointer + 1], registerA, registerB, registerC);
+						registerA /= denominator;
+					}
 
-				instructionPointer += 2;
-				break;
-			}
-			case OPCODE_BXL:
-			{
-				// The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand,
-				// then stores the result in register B.
-				if (instructionPointer + 1 < programVector.count)
-				{
-					registerB ^= programVector.data[instructionPointer + 1];
-				}
-
-				instructionPointer += 2;
-				break;
-			}
-			case OPCODE_BST:
-			{
-				// The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby keeping only its lowest 3 bits),
-				// then writes that value to the B register.
-				if (instructionPointer + 1 < programVector.count)
-				{
-					registerB = comboOperand(programVector.data[instructionPointer + 1], registerA, registerB, registerC) % 8;
-				}
-
-				instructionPointer += 2;
-				break;
-			}
-			case OPCODE_JNZ:
-			{
-				// The jnz instruction (opcode 3) does nothing if the A register is 0.
-				// However, if the A register is not zero, it jumps by setting the instruction pointer to the value of its literal operand;
-				// if this instruction jumps, the instruction pointer is not increased by 2 after this instruction.
-				if (registerA != 0 && instructionPointer + 1 < programVector.count)
-				{
-					instructionPointer = programVector.data[instructionPointer + 1];
-				}
-				else
-				{
 					instructionPointer += 2;
+					break;
 				}
-				break;
-			}
-			case OPCODE_BXC:
-			{
-				// The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the result in register B.
-				// (For legacy reasons, this instruction reads an operand but ignores it.)
-				if (instructionPointer + 1 < programVector.count)
+				case OPCODE_BXL:
 				{
-					registerB = registerB ^ registerC;
-				}
+					// The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand,
+					// then stores the result in register B.
+					if (instructionPointer + 1 < programVector.count)
+					{
+						registerB ^= programVector.data[instructionPointer + 1];
+					}
 
-				instructionPointer += 2;
-				break;
-			}
-			case OPCODE_OUT:
-			{
-				// The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value.
-				// (If a program outputs multiple values, they are separated by commas.)
-				if (instructionPointer + 1 < programVector.count)
+					instructionPointer += 2;
+					break;
+				}
+				case OPCODE_BST:
 				{
-					addVector(&outVector, comboOperand(programVector.data[instructionPointer + 1], registerA, registerB, registerC) % 8);
-				}
+					// The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby keeping only its lowest 3 bits),
+					// then writes that value to the B register.
+					if (instructionPointer + 1 < programVector.count)
+					{
+						registerB = comboOperand(programVector.data[instructionPointer + 1], registerA, registerB, registerC) % 8;
+					}
 
-				instructionPointer += 2;
-				break;
-			}
-			case OPCODE_BDV:
-			{
-				// The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the B register.
-				// (The numerator is still read from the A register.)
-				if (instructionPointer + 1 < programVector.count)
+					instructionPointer += 2;
+					break;
+				}
+				case OPCODE_JNZ:
 				{
-					int denominator = 1 << comboOperand(programVector.data[instructionPointer + 1], registerA, registerB, registerC);
-					registerB = registerA / denominator;
+					// The jnz instruction (opcode 3) does nothing if the A register is 0.
+					// However, if the A register is not zero, it jumps by setting the instruction pointer to the value of its literal operand;
+					// if this instruction jumps, the instruction pointer is not increased by 2 after this instruction.
+					if (registerA != 0 && instructionPointer + 1 < programVector.count)
+					{
+						instructionPointer = programVector.data[instructionPointer + 1];
+					}
+					else
+					{
+						instructionPointer += 2;
+					}
+					break;
 				}
-
-				instructionPointer += 2;
-				break;
-			}
-			case OPCODE_CDV:
-			{
-				// The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C register.
-				// (The numerator is still read from the A register.)
-				if (instructionPointer + 1 < programVector.count)
+				case OPCODE_BXC:
 				{
-					int denominator = 1 << comboOperand(programVector.data[instructionPointer + 1], registerA, registerB, registerC);
-					registerC = registerA / denominator;
-				}
+					// The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C, then stores the result in register B.
+					// (For legacy reasons, this instruction reads an operand but ignores it.)
+					if (instructionPointer + 1 < programVector.count)
+					{
+						registerB = registerB ^ registerC;
+					}
 
-				instructionPointer += 2;
-				break;
+					instructionPointer += 2;
+					break;
+				}
+				case OPCODE_OUT:
+				{
+					// The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value.
+					// (If a program outputs multiple values, they are separated by commas.)
+					if (instructionPointer + 1 < programVector.count)
+					{
+						addVector(&outVector, comboOperand(programVector.data[instructionPointer + 1], registerA, registerB, registerC) % 8);
+					}
+
+					instructionPointer += 2;
+					break;
+				}
+				case OPCODE_BDV:
+				{
+					// The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the B register.
+					// (The numerator is still read from the A register.)
+					if (instructionPointer + 1 < programVector.count)
+					{
+						long long denominator = 1LL << comboOperand(programVector.data[instructionPointer + 1], registerA, registerB, registerC);
+						registerB = registerA / denominator;
+					}
+
+					instructionPointer += 2;
+					break;
+				}
+				case OPCODE_CDV:
+				{
+					// The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C register.
+					// (The numerator is still read from the A register.)
+					if (instructionPointer + 1 < programVector.count)
+					{
+						long long denominator = 1LL << comboOperand(programVector.data[instructionPointer + 1], registerA, registerB, registerC);
+						registerC = registerA / denominator;
+					}
+
+					instructionPointer += 2;
+					break;
+				}
 			}
 		}
+
+		/*
+		printf("%lld - ", initialA);
+		for (int outIndex = 0; outIndex < outVector.count - 1; outIndex++)
+		{
+			printf("%d, ", outVector.data[outIndex]);
+		}
+		printf("%d\n", outVector.data[outVector.count - 1]);
+		*/
+		if (memcmp(outVector.data, &programVector.data[programVector.count - numCompareDigits], sizeof(int) * numCompareDigits) == 0)
+		{
+			if (programVector.count == outVector.count)
+			{
+				break;
+			}
+			initialA *= 8;
+			numCompareDigits++;
+		}
+		else
+		{
+			initialA++;
+		}
+
+		instructionPointer = 0;
+		clearVector(&outVector);
 	}
 
-	for (int outIndex = 0; outIndex < outVector.count - 1; outIndex++)
-	{
-		printf("%i,", outVector.data[outIndex]);
-	}
-	printf("%i", outVector.data[outVector.count - 1]);
+	assert(programVector.count == outVector.count && memcmp(programVector.data, outVector.data, sizeof(int)* programVector.count) == 0);
+	printf("%lld\n", initialA);
 }
